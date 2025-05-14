3d39b81d2106ad16ebbf020866e95e9565bfd1af
Jongwon Lee <jlee678@my.centennialcollege.ca>
2025-05-13 21:28:18 -0400

    add logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import logo from "../../logo.jpg";
+
 
 export default function Layout() {
     return (
         <>
             <h1>My Portfolio</h1>
+            <img src={logo} alt="Logo" />
+            
             <nav>
-                <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/projects">Projects</Link> | <Link to="/services">Services</Link> | <Link to="/contact">Contact</Link>
+                <Link to="/">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/projects">Projects</Link> | <Link to="/services">Services</Link> | <Link to="/contact">Contact</Link>
             </nav>
         </>
     )
