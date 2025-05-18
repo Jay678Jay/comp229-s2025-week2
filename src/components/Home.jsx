431a828a4b791ca23b2305fb1af53def4a7693a0
Jongwon Lee <jlee678@my.centennialcollege.ca>
2025-05-18 00:28:50 -0400

    modify home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
         <h1>Home</h1>
         <p style={{ fontSize: '30px' }}><strong>Welcome to my portfolio!</strong></p>
+        <p style={{ fontSize: '20px' }}>This portfolio showcases my skills and projects. As a student, my mission is to apply what I learn to build functional, user-friendly applications and to grow as a developer by continuous learning and hands on experience.</p>
     </div>
 )
 }
