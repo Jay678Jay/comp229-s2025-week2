677ebb0713eb429ff37ed67b2963a144bd5a7366
Jongwon Lee <jlee678@my.centennialcollege.ca>
2025-05-19 00:26:01 -0400

    edit file location

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@
 // Date: May 18, 2025
 
 
-// About page with personal information, imaeg and resume link
+// About page with personal information, image and resume link
 
 import image from '../assets/image.png';
 
@@ -14,7 +14,7 @@ export default function About() {
         <h1>About Me</h1>
         <img src={image} alt="Image" className="image" />
         <p style={{ fontSize: '20px' }}>My name is Jongwon Lee, and I'm in my third semester of the Software Engineering program at Centennial College. <br /> My goal is to pursue a career as a software developer.</p>
-        <a href="../../resume.pdf">Link to my resume</a>
+        <a href="../resume.pdf">Link to my resume</a>
     </div>
 )
 }
